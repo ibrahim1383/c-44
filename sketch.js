9105282ef5e7f911e34ad7d15f0587ac001b86d7
ibrahim1383 <64684809+ibrahim1383@users.noreply.github.com>
2020-09-12 15:02:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var a1,a2,a3,a4,a4
+function setup(){
+  createCanvas(800,800);
+
+a1=createSprite(45,800,10,120)
+a1=createSprite(120,800,10,220)
+
+
+}
+
+
+
+function draw() {
+  background(0);
+  
+  
+  text("x:"+mouseX+"  y:"+mouseY,mouseX,mouseY);
+  drawSprites();
+  }
+
+  
+  
+
+ 
+
